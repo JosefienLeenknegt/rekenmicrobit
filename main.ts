ffaf1311c95fa1b471c12f2050e032247653121f
JosefienLeenknegt <90095946+JosefienLeenknegt@users.noreply.github.com>
2021-09-18 15:30:58 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Josefien += 1
+    basic.showNumber(Josefien)
+})
+input.onButtonPressed(Button.AB, function () {
+    Josefien = 0
+    basic.showNumber(Josefien)
+})
+input.onButtonPressed(Button.B, function () {
+    Josefien += -1
+    basic.showNumber(Josefien)
+})
+let Josefien = 0
+Josefien = 0
+basic.showNumber(Josefien)
